01e45c6f665de1e4a6c21bdf15740953c67861c3
Benjamin Jung <bsjung@gmail.com>
2020-02-10 00:13:44 +0900

    Update k_chart  - add k_line.

--- lib/flutter_k_chart.dart
+++ lib/flutter_k_chart.dart
@@ -1,8 +1,10 @@
 library flutter_k_chart;
 
-export 'k_chart_widget.dart'; //layout
 export 'utils/data_util.dart'; //Data processing
 export 'entity/k_line_entity.dart'; //data
 
 export 'entity/depth_entity.dart';
 export 'depth_chart.dart';
+
+export 'k_chart_widget.dart'; //chart
+//export 'k_line_widget.dart';  //line
